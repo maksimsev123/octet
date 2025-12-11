f633445406851d6bb4744f4306ea8ab3aa260214
maksim <maksimsevostianov200714@gmail.com>
2025-12-11 16:56:35 +0300

    Initial commit: text analyzer project

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod text_stats;
 use text_stats::text_stats::count_chars;
 
 fn main() {
-    let text = "Hello!";
+    let text = "Sevostyanov Maksim Pavlovich!";
     let chars = count_chars(text);
     println!("Количество символов: {}", chars);
 }

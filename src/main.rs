a9dea24146709da4cdfc6dc22ac477a7f36ef7d9
maksim <maksimsevostianov200714@gmail.com>
2025-12-18 15:50:32 +0300

    Добавлен анализатор текста с подсчетом строк и слов

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,21 @@ mod text_stats;
 use text_stats::text_stats::count_chars;
 
 fn main() {
-    let text = "Sevostyanov Maksim Pavlovich!";
-    let chars = count_chars(text);
-    println!("Количество символов: {}", chars);
+    // Упражнение 1
+    println!("One: {} строк", "One".lines().count());
+    println!("One\\nTwo: {} строк", "One\nTwo".lines().count());
+    println!("One\\nTwo\\nThree\\n: {} строк", "One\nTwo\nThree\n".lines().count());
+    println!("\"\": {} строк", if "".is_empty() { 0 } else { "".lines().count() });
+    
+    // Упражнение 2
+    println!("Rust language: {} слов", "Rust language".split(' ').count());
+    println!("Hello, world!!!: {} слов", "Hello, world!!!".split(' ').count());
+    println!("Мне 18 лет: {} слов", "Мне 18 лет".split(' ').count());
+    println!("123 456 789: {} слов", "123 456 789".split(' ').count());
+    println!("...: {} слов", "...".split(' ').count());
+    
+    // Упражнение 3
+    println!("Rust❤️2025: {} слов", "Rust❤️2025".split(' ').count());
+    println!("Привет,   мир!   😀🌍: {} слов", "Привет,   мир!   😀🌍".split(' ').filter(|s| !s.is_empty()).count());
+    println!("a\\t\\t\\tb\\n\\n\\nc🚀🚀🚀: {} слов", "a\t\t\tb\n\n\nc🚀🚀🚀".split_ascii_whitespace().count());
 }
